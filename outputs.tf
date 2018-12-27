5c00963989c43ee8be9b0999748709a6940ab90f
John Lin <johnlin@exosite.com>
2018-12-27 17:27:50 +0800

    use splat in output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "sns_topic_arn" {
   description = "The ARN of the SNS topic"
-  value       = "${aws_sns_topic.default.arn}"
+  value       = "${var.topic_arn == "" ? aws_sns_topic.default.*.arn[0] : var.topic_arn}"
 }
